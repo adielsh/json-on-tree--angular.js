ab27791db0b0e1cd41a23f96ba2d05477baf9a49
Igor Deryabin <igor@rodweb.ru>
2017-10-27 16:51:09 +0200

    Allow fetching data from an external source

--- start.js
+++ start.js
@@ -6,10 +6,11 @@ const server = http.createServer(app);
 
 app.get('/', function(req,res) {
     res.sendfile('public/index.html');
-  });
+});
 app.use('/static-scripts', express.static(__dirname + '/node_modules'));
 app.use('/js', express.static(__dirname + '/public/js'));
 app.use('/css', express.static(__dirname + '/public/css'));
+app.use('/data', express.static(__dirname + '/public/data'));
 
 server.listen(process.env.PORT || 3000, () => {
     console.log('server listens to port 3000');
